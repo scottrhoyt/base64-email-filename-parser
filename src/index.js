22cc8ce3068ba549cb32198dab6621011d3a1c0a
Scott Hoyt <scottrhoyt@gmail.com>
2017-06-01 19:20:18 -0700

    Add ability to validate extensions and test.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@
 var base64 = require('base-64');
 var validator = require("email-validator");
 
-module.exports = function(fileName) {
+module.exports = function(fileName, acceptedExtensions) {
+  var acceptedExtensions = acceptedExtensions || [];
   var decodedFileName =  decodeURIComponent(fileName.replace(/\+/g, " ")); //the object may have spaces
+  var components = decodedFileName.split('.');
+
+  if (components.length != 3) {
+    throw new Error('The file name did not have 3 components.');
+  }
+
+  if (acceptedExtensions.isArray == false) {
+    acceptedExtensions = [acceptedExtensions];
+  }
+
+  if (acceptedExtensions.includes(components[2]) == false) {
+    throw new Error('The file name does not have an accepted extension.');
+  }
+
   var emailEncoded = decodedFileName.split('.')[0];
   var emailDecoded = base64.decode(emailEncoded);
 
--- test/test.js
+++ test/test.js
@@ -2,40 +2,50 @@ var assert = require('assert');
 var emailExtractor = require('../src/index');
 
 describe('emailExtractor', () => {
-  it('should decode an email properly', () => {
-    var encoded = 'am9obi5kb2VAZW1haWwuY29t';
-    var expected = 'john.doe@email.com';
-    assert.equal(expected, emailExtractor(encoded));
-  });
-
-  it('should decode an email as the first part of a fileName properly', () => {
+  it('should decode an email as the first part of a fileName properly with one accepted extension', () => {
     var encoded = 'am9obi5kb2VAZW1haWwuY29t.1.txt';
     var expected = 'john.doe@email.com';
-    assert.equal(expected, emailExtractor(encoded));
+    assert.equal(expected, emailExtractor(encoded, 'txt'));
+  });
+
+  it('should decode an email as the first part of a fileName properly with an array of accepted extensions', () => {
+    var encoded = 'am9obi5kb2VAZW1haWwuY29t.1.txt';
+    var expected = 'john.doe@email.com';
+    assert.equal(expected, emailExtractor(encoded, ['txt', 'dat']));
   });
 
   it('should throw an error if the string cannot be decoded', () => {
-    var encoded = 'am9obi5kb2VAZW1haWwuY29*(';
-    assert.throws(()=>emailExtractor(encoded));
+    var encoded = 'am9obi5kb2VAZW1haWwuY29*(.1.txt';
+    assert.throws(()=>emailExtractor(encoded, 'txt'));
   });
 
   it('should throw an error if the decoded string is not an email', () => {
-    var encoded = 'dGhpc2lzbm90YW5lbWFpbA==';
-    assert.throws(()=>emailExtractor(encoded));
+    var encoded = 'dGhpc2lzbm90YW5lbWFpbA==.1.txt';
+    assert.throws(()=>emailExtractor(encoded, 'txt'));
   });
 
   it('should throw an error if passed an empty string', () => {
     var encoded = '';
-    assert.throws(()=>emailExtractor(encoded));
+    assert.throws(()=>emailExtractor(encoded, 'txt'));
   });
 
   it('should throw an error if passed null', () => {
     var encoded = null;
-    assert.throws(()=>emailExtractor(encoded));
+    assert.throws(()=>emailExtractor(encoded, 'txt'));
   });
 
   it('should throw an error if passed undefined', () => {
     var encoded = undefined;
-    assert.throws(()=>emailExtractor(encoded));
+    assert.throws(()=>emailExtractor(encoded, 'txt'));
+  });
+
+  it('should throw an error if the filename does not have three components', () => {
+    var encoded = 'am9obi5kb2VAZW1haWwuY29t.txt';
+    assert.throws(()=>emailExtractor(encoded, 'txt'));
+  });
+
+  it('should throw an error if the filename does not have an accepted extension', () => {
+    var encoded = 'am9obi5kb2VAZW1haWwuY29t.dat';
+    assert.throws(()=>emailExtractor(encoded, 'txt'));
   });
 });
